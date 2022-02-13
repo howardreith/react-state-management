6c06df31b3771277b60bb9efb664adf0af3c1a77
howardreith <howard.reith@gmail.com>
2022-02-13 16:39:19 -0500

    remove extraneous title row

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -5,11 +5,6 @@ import PirateTicTacToe from "./PirateTicTacToe";
 export default function App() {
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div className="jumbotron col-10 offset-1">
-                    <h1>Pirate Tic Tac Toe</h1>
-                </div>
-            </div>
             <AppProvider>
                 <PirateTicTacToe/>
             </AppProvider>
